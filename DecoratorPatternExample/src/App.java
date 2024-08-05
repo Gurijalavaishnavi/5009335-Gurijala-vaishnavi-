6862e8391ec5d916fa14b9bce4c0ca083ddabb9c
Gurijalavaishnavi <143484871+Gurijalavaishnavi@users.noreply.github.com>
2024-08-05 16:52:14 +0530

    Delete DecoratorPatternExample directory

--- DecoratorPatternExample/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Notifier notifier=new SlackNotifierDecorator(new SMSNotifierDecorator(new EmailNotifier()));
-        System.out.println("Notification sent on : "+notifier.send());
-    }
-}
